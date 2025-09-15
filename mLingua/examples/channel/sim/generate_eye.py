877a14bf27e0b96c5398f972baa4288d6da06d9e
raytroop <raytroop@gmail.com>
2025-09-16 00:02:29 +0800

    IUSHOME -> XLMHOME

--- mLingua/examples/channel/sim/generate_eye.py
+++ mLingua/examples/channel/sim/generate_eye.py
@@ -47,7 +47,7 @@ def run(filename, imgname, period, time_offset, nbin):
   #cax=plt.imshow(H/s,interpolation='gaussian',cmap=plt.cm.coolwarm, extent=extent, norm=Normalize(), aspect=aspect)
   plt.grid(color='w')
   fig.colorbar(cax)
-  ax.set_axis_bgcolor('k')
+  ax.set_facecolor('k')
 
   plt.savefig(imgname)
 
